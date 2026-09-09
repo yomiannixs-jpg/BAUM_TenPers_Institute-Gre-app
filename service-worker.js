cd49e8d6c36ce014b9d1772fb77fa15c75d7ce4c
Yomi Annixs <yomiannixs@gmail.com>
2026-09-09 17:20:48 +0100

    BAUM TenPers GRE 330+ production web app

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE='baum-gre-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./share.html'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+    if(e.request.method==='GET' && new URL(e.request.url).origin===self.location.origin){
+      const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));
+    }
+    return resp;
+  }).catch(()=>caches.match('./index.html'))));
+});
