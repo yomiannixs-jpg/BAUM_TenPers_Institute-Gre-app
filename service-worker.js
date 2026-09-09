96a9847a3ba007f4536b8d00b4585e664b4eb80b
Yomi Annixs <yomiannixs@gmail.com>
2026-09-09 19:58:34 +0100

    Upgrade BAUM TenPers GRE to v2.7 Server Side Question Bank

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='baum-gre-v2-3-adaptive';
+const CACHE='baum-gre-v2-7-server-bank';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./share.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
