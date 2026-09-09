ad56e6eb8d46522aca3095ef42c958ebb2751611
Yomi Annixs <yomiannixs@gmail.com>
2026-09-09 20:09:07 +0100

    Upgrade BAUM TenPers GRE to v2.9 Full GRE Simulation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='baum-gre-v2-8-quality';
+const CACHE='baum-gre-v2-9-full-sim';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./share.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
