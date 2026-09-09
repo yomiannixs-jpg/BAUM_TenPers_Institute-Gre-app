d0042784aa4ad05cfd0372cca2e2664108756695
Yomi Annixs <yomiannixs@gmail.com>
2026-09-09 18:17:04 +0100

    Upgrade BAUM TenPers GRE to v2.0 with 5000 questions

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='baum-gre-v1';
+const CACHE='baum-gre-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./share.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
