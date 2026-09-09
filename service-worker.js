8ab148fa0baf8af3f95201da8a0392085128c4b9
Yomi Annixs <yomiannixs@gmail.com>
2026-09-09 20:13:31 +0100

    Upgrade BAUM TenPers GRE to v3.0 Institutional Platform

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='baum-gre-v2-9-full-sim';
+const CACHE='baum-gre-v3-0-institutional';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./share.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
