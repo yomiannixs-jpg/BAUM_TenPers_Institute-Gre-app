5b271bd7d379e511adab2d58f33bcc4d38973616
Yomi Annixs <yomiannixs@gmail.com>
2026-09-09 18:59:40 +0100

    Upgrade BAUM TenPers GRE to v2.2 Secure with test selector

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='baum-gre-v2-1-secure';
+const CACHE='baum-gre-v2-2-secure';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./share.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
