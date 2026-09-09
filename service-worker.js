e55b09ee57aa7732ab4ab9addb84fca2c44b92c3
Yomi Annixs <yomiannixs@gmail.com>
2026-09-09 19:25:57 +0100

    Upgrade BAUM TenPers GRE to v2.3 Adaptive Score Engine

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='baum-gre-v2-2-secure';
+const CACHE='baum-gre-v2-3-adaptive';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./share.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
